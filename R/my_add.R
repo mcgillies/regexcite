e643374f3a902d8107b9b16bbdf15116d37aedd6
Matthew Gillies <mcgillies@shaw.ca>
2023-03-31 12:42:18 -0700

    Create my_add function

---
+++ R/my_add.R
@@ -0,0 +1,24 @@
+#' Add two numbers
+#'
+#' @param an integer, and another second optional integer
+#'
+#' @return An integer, NA or error message
+#' @export
+#'
+#' @examples
+#' x <- 1
+#' y <- 2
+#' my_add(x,y)
+#' my_add(x)
+#' my_add(99, 103)
+my_add <- function(x, y=10) {
+  if (is.na(x) || is.na(y)) {
+    return(NA)
+  } else if (is.character(x) || is.character(y)) {
+    stop("One of your inputs contains a string")
+  } else if (missing(y)){
+    return(x+y)
+  } else {
+    return(x+y)
+  }
+}
